b10e700eb5a50eb32fca6b9fcbdc06ac109080c9
kikarin <akunamazon4811@gmail.com>
2026-08-08 11:39:23 +0700

    fix test sekali lagi

--- web/tests/TestCase.php
+++ web/tests/TestCase.php
@@ -6,5 +6,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Feature tests don't need a real Vite build; CI has no public/build/manifest.json.
+        $this->withoutVite();
+    }
 }
